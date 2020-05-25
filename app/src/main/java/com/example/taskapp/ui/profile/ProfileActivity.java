d9c4bb3a086a526a2d5ee8caae81a5f0ce404b26
aidinrakanov <altynaikanatbekova@gmail.com>
2020-05-25 21:28:29 +0600

    2

--- app/src/main/java/com/example/taskapp/ui/profile/ProfileActivity.java
+++ app/src/main/java/com/example/taskapp/ui/profile/ProfileActivity.java
@@ -1,19 +1,39 @@
 package com.example.taskapp.ui.profile;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.taskapp.R;
+import com.example.taskapp.models.User;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ProfileActivity extends AppCompatActivity {
+    EditText editName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
+        editName = findViewById(R.id.editName);
+       // getData();
+        getData2();
         if (getSupportActionBar() != null) {
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
             getSupportActionBar().setDisplayShowHomeEnabled(true);
@@ -24,5 +44,55 @@ public class ProfileActivity extends AppCompatActivity {
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
         finish();
         return true;
+    }
+
+//    private void getData(){
+//        String uid  = FirebaseAuth.getInstance().getUid();
+//        FirebaseFirestore.getInstance()
+//                .collection("users").document(uid)
+//                .get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//            @Override
+//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                if(task.isSuccessful()){
+//                    User user = task.getResult().toObject(User.class);
+//                    editName.setText(user.getName());
+//                }
+//            }
+//        });
+//    }
+    private void getData2(){
+        String uid  = FirebaseAuth.getInstance().getUid();
+        FirebaseFirestore.getInstance()
+                .collection("users").document(uid)
+                .addSnapshotListener(new EventListener<DocumentSnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable DocumentSnapshot documentSnapshot, @Nullable FirebaseFirestoreException e) {
+                        if (documentSnapshot.exists()){
+                            User user = documentSnapshot.toObject(User.class);
+                            editName.setText(user.getName());
+                        }
+                    }
+                });
+    }
+
+    public void save_profile(View view) {
+        String uid  = FirebaseAuth.getInstance().getUid();
+        String name = editName.getText().toString().trim();
+        User user = new User("Aidin",26, null);
+        FirebaseFirestore.getInstance()
+                .collection("users")
+                .document(uid)
+                .set(user)
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        if (task.isSuccessful()){
+                            Toast.makeText(ProfileActivity.this, "Успешно",
+                                    Toast.LENGTH_SHORT).show();
+                        }else {Toast.makeText(ProfileActivity.this, "Ошибка",
+                                Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
